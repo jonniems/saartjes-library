cb60cf7b491f42dcdc0a609dba6d3cd8176dc08e
Jonnie Muis <jonnie@forloyalty.com>
2025-09-30 11:03:21 +0200

    Add build configuration to vite.config.js to set assets inline limit to 100 KB

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // standaard is 4 KB
+    assetsInlineLimit: 100000, // bijv. 100 KB
+  },
+});
