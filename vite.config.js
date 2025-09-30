b6f14ef8f846c58a291616dd679b4e806c6ccbe5
Jonnie Muis <jonnie@forloyalty.com>
2025-09-30 13:29:08 +0200

    Update Vite configuration to set base path for deployment and remove flex display from library book container links for improved styling.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/saartjes-library/ditisjecadeau.nl/',
   build: {
     // standaard is 4 KB
     assetsInlineLimit: 100000, // bijv. 100 KB
