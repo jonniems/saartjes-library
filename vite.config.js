308992e25cdd9dcda87312c3ac4250dae0ded516
Jonnie Muis <jonnie@forloyalty.com>
2025-10-01 09:14:35 +0200

    Remove base path configuration from Vite setup to allow for dynamic deployment paths.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/saartjes-library/',
   build: {
     // standaard is 4 KB
     assetsInlineLimit: 100000, // bijv. 100 KB
